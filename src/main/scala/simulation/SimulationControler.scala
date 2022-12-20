c5c0b554783fbbcd6f9df7781fe13201494f62d2
Jens Berling <jens.lucas@gmail.com>
2022-12-20 11:48:05 +0100

    Mainly refractor

--- src/main/scala/simulation/SimulationControler.scala
+++ src/main/scala/simulation/SimulationControler.scala
@@ -5,22 +5,21 @@ import window.Window
 
 import java.awt.event.KeyEvent
 
-case class SimulationViewer(width: Int, height: Int):
-  val window = Window("Simulation Viewer", width, height)
-  val simulation = Simulation()
-  var currentPosition = Position(0, 0)
+case class SimulationControler(width: Int, height: Int):
+  val window: Window = Window("Simulation Viewer", width, height)
+  val simulation: Simulation = Simulation()
+  var currentPosition: Position = Position(0, 0)
 
 
   def run(): Unit =
     while true do
       simulation.tick(5)
       tickCamera()
-      window.render { g2d =>
-        simulation.gameObjects.foreach {
-          case creature: Drawable => creature.draw(g2d)
-          case _ => println("WOOP")
-        }
-      }
+      window.render(g2d => {
+        simulation.gameObjects.foreach(creature => creature.draw(g2d))
+      })
+
+
 
   private def tickCamera(): Unit =
     if window.keyManager.isKeyPressed(KeyEvent.VK_UP) then currentPosition = currentPosition + Position(0, 10)
@@ -29,5 +28,5 @@ case class SimulationViewer(width: Int, height: Int):
     else if window.keyManager.isKeyPressed(KeyEvent.VK_RIGHT) then currentPosition = currentPosition + Position(-10, 0)
 
     window.setOrigin(currentPosition)
-    Thread.sleep(10)
+    Thread.sleep(10) //TODO Varför är timern här
     println(s"Camera offset = $currentPosition, MousePos = ${window.mousePos}")
